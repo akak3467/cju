23851353d69d7e643be2cbd0b528e6c80e2e17d8
cju <akak3467@naver.com>
2021-02-02 15:47:39 +0900

    클래스형 컴포넌트의 state

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-const MyComponent = ({name ,패이브, children}) => {
-    return (
-      <div>
-          안녕하세요 제이름은 {name} 입니다 <br/>
-          children 값은 {children}입니다
-          <br/>
-          제가 좋아하는 숫자는 {패이브}입니다.
-      </div>
-    );
-}; 
-MyComponent.defaultPoros = {
-    name: '기본 이름'
-};
+import React, { Component } from 'react';
 
-MyComponent.PropTypes = {
-    name: PropTypes.string,
-    패이브:PropTypes.number.isRequired
-};
-
-export default MyComponent;
+ 
+ class Counter extends Component {
+     constructor(props) {
+         super(props);
+         this.state = {
+            number: 0
+         };
+ } 
+ render() {
+     const { number} = this.state;
+     return (
+         <div>
+             <h1>{number}</h1>
+             <button
+             onClick={() => {
+                 this.setState({ number: number +1 });
+             }}
+             >
+                 +1
+             </button>
+         </div>
+     );
+    }
+ }
+ export default Counter;
